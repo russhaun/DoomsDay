4fd7d8ceb450a13be64c34efcad53ee4a0757043
Russ Haun <russhaun@verizon.net>
2018-09-16 14:00:17 -0400

    docker repo sync:
    updated files to reflect docker image.  this image is built off of alpine 3.7 python 2.7  small demo

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
-FROM ubuntu:latest
-MAINTAINER oldkingcone(Iamroot)
-RUN apt-get update -y
-RUN apt-get install -y python-pip python-dev build-essential apache aespipe
-RUN service apache2 start
-COPY ./app
+FROM alpine:3.7
+MAINTAINER rhaun72
+RUN apk add --no-cache alpine-sdk util-linux pciutils usbutils binutils findutils grep coreutils python python2-dev py-pip openrc
+COPY . ./app
 WORKDIR /app
+RUN pip install --upgrade pip
 RUN pip install -r REQUIREMENTS
 ENTRYPOINT ["python"]
 CMD ["DoomsDayMain.py"]
--- DoomsDayMain.py
+++ DoomsDayMain.py
@@ -1,6 +1,10 @@
 # decided to go with twisted, much more easy to work with.
 
 try:
+    import itertools
+    import random
+    import email_chooser
+    from faker import Faker
     import sqlite3
     import names
     import hashlib
@@ -11,54 +15,69 @@ try:
     import os
     from time import sleep
     from random import randint
+    from socket import AF_INET, socket, SOCK_STREAM
+    from threading import Thread
+
 except ImportError as e:
-    print(
-        "Sorry... Something went wrong, try running pip install -r REQUIREMENTS and run the app again. \n {}".format(e))
+    print("[**] Sorry... Something went wrong, try running pip install -r REQUIREMENTS and run the app again. \n {}".format(e))
     import sys
     sys.exit(1)
+FileExistsError = ""
+print("[**] DoomsDay triggered beginning setup")
+print("[**] Making falsified www root directory")
+if os.path.isdir("./wwwroot"):
+    print("[**] Directory exists....continuing")
+    os.system("cd ./wwwroot")
+else:
+    print("[**] Directory does not exist....exiting")
+    pass
 
-os.system("mkdir ./wwwroot")
-os.system("cd ./wwwroot")
-
-database = sqlite3.connect('admin_databse.sqlite')
+database = sqlite3.connect('./wwwroot/admin_databse.sqlite')
 c = database.cursor()
-#creating the junk filled and falsely encrypted database(make it mimic a glob)
+
 #@todo database is complete, just missing the email address generation.
 #@todo make a relational database, add some more hashes maybe sha1 and faked access times.
-#@todo, alter the join times to reflect at least a month back in time.
-c.execute('''CREATE TABLE IF NOT EXISTS Site_Info(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, jointime TIMESTAMP DEFAULT 
-            CURRENT_TIMESTAMP NOT NULL, username TEXT, email TEXT, password TEXT)''')
-#@todo, ok so this works. Need to find a way to populate legit looking IP address's
-#@todo
-''' or just take all the IP address's from lastb and cram them in here, 
-    that way it all resolves back to "legit" sources.'''
+c.execute('''CREATE TABLE IF NOT EXISTS Site_Info(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+                username TEXT, email TEXT, password TEXT)''')
 c.execute('''CREATE TABLE IF NOT EXISTS Access_times(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT,
-            FORIGN_KEY email REFERENCES username, Accessed_Last CURRENT_TIMESTAMP NOT NULL, Login_IP TEXT)''')
-sql_stmt = "INSERT INTO Site_Info(username, email, password) VALUES ('%s', '%s', '%s')"
-sql_stmt = str(sql_stmt)
+            FORIGN_KEY email REFERENCES username, login_ip TEXT)''')
 
-def name_generate():
-    digest_name = "INSERT INTO Site_Info(username, password) VALUES ('%s', '%s')"
-    namer = set()
-    for i in range(100):
+def name_generate(length):
+    print("[**] Generating false database.......")
+    ip_net = list()
+    digest_name = "INSERT INTO Site_Info(username, email, password) VALUES ('%s', '%s', '%s')"
+    accessed_table = "INSERT INTO Access_times(login_ip) VALUES ('%s')"
+    fake_names = list()
+    for i in range(length):
+        faker = Faker()
+        ip_addr = faker.ipv4()
+        ip_net.append(str(ip_addr))
         name = names.get_full_name()
-        namer.add(name)
-        print(name)
-        database.commit()
-        style = name.encode()
-        hashed_pass = hashlib.sha512(style).hexdigest()
-        c.execute(digest_name % (name, hashed_pass))
-        database.commit()
-
-
-name_generate()
+        styled_names = name.encode('utf-8')
+        fake_names.append(styled_names)
+   
+    for (emails, fakes, ips) in itertools.izip_longest(email_chooser.genEmail(length), fake_names, ip_net):
+        hashed_pass = hashlib.sha1(fakes).hexdigest()
+        #print('{} \n {} \n {}\n {}\n'.format(emails, fakes, ips, hashed_pass))
+        fake = fakes.decode()
+        c.execute(digest_name % (fake, emails, hashed_pass))
+        c.execute(accessed_table % (ips))
+    database.commit()
+#
+#while alcohol == true: break
+length = random.randint(100, 400)
+name_generate(length)
+print("[**] false ips, names, emails generated")
 c.close()
 
-
+print("[**] false database created!!!!!!!")
 #@todo set up on connection recieved, so we are not hogging precious system resources.
+# going to start working on tihs tomorrow at some point. Yall be patient.
+#
 
 resource = File('./wwwroot')
 factory = Site(resource)
-endpoint = endpoints.TCP4Server(reactor, 8888)
+endpoint = endpoints.TCP4ServerEndpoint(reactor, 8888)
 endpoint.listen(factory)
+print("[**] DoomsDay Websrv is running")
 reactor.run()
